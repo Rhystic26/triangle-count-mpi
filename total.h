57495e062d0c470a639af1acbeeb8bd142200ada
Rhystic26 <128982075+Rhystic26@users.noreply.github.com>
2025-04-08 16:04:45 -0400

    Added total.h file.

---
+++ total.h
@@ -0,0 +1,36 @@
+// #pragma once
+#include <gms/common/types.h>
+#include <cassert>
+#include <mpi.h>
+
+namespace GMS::TriangleCount::Par {
+
+template<class SGraph>
+size_t count_total(const SGraph &graph) {
+  int rank, gsize, csize, gtotal;
+  size_t n = graph.num_nodes();
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &gsize);
+  csize = n / gsize;
+
+    size_t total = 0;
+    // #pragma omp parallel for schedule(static, 17) reduction(+:total)
+    for (NodeId u = rank*csize; u < (rank+1)*csize; ++u) {
+        const auto &neigh_u = graph.out_neigh(u);
+        for (NodeId v : neigh_u) {
+            if (u < v) {
+                total += neigh_u.intersect_count(graph.out_neigh(v));
+            }
+        }
+    }
+
+    MPI_Reduce(&total, &gtotal, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+      assert(gtotal % 3 == 0);
+    }
+    
+    return gtotal / 3;
+}
+
+}
